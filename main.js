3d6df28a07e9e0b1d6389f69d50ecf15f68c78c8
RinPark1512 <112235129+RinPark1512@users.noreply.github.com>
2024-06-13 14:27:31 -0700

    editted frontend, created javascript

---
+++ main.js
@@ -0,0 +1,21 @@
+// when content loaded, run event
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function Api.");
+        count = response.count; //assign variable as the json response
+        document.getElementById("counter").innerText = count; // go into doc, find counter, 
+        //find inner text and set it accordingly
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
